818c55d53cb4db89a92da0db2e92af2fd194d4fc
Farid <fareedibrahim0003@gmail.com>
2023-09-16 20:22:48 +0100

    admin all doctors

--- src/pages/AdminDashboard/Doctor/index.jsx
+++ src/pages/AdminDashboard/Doctor/index.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Sidebar from '@/components/Sidebar/Sidebar';
 import Navbar from '@/components/Navbar/Navbar';
 import styles from "./Doctor.module.css";
+import { BiSearch } from "react-icons/bi"
+import { BsTrashFill } from "react-icons/bs"
+import { FiEdit2 } from "react-icons/fi"
+import { GrFormNextLink, GrFormPreviousLink } from "react-icons/gr"
+
+
 
 const Index = () => {
+
+    useEffect(() => {
+        // Initialize Bootstrap tabs using JavaScript when the component mounts
+        const tabs = new window.bootstrap.Tab(document.getElementById('menu0'));
+        tabs.show();
+    }, []);
+
     return (
         <div className={styles.body}>
             <Sidebar />
@@ -11,8 +24,125 @@ const Index = () => {
                 <Navbar />
                 {/* body content start */}
 
-                <h1>welcome doctor</h1>
-               
+                <div className={styles.content}>
+                    <ul className="nav nav-tabs" id="myTabs">
+                        <li className="nav-item">
+                            <a className={`nav-link active ${styles.navLink}`} id="menu0-tab" data-bs-toggle="tab" href="#menu0" role="tab" aria-controls="menu0" aria-selected="true">Doctors</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className={`nav-link ${styles.navLink}`} id="menu1-tab" data-bs-toggle="tab" href="#menu1" role="tab" aria-controls="menu1" aria-selected="false">Add Doctor</a>
+                        </li>
+                    </ul>
+
+                    <div className={`row ${styles.searchNextDiv}`}>
+                        <div className={`col-6 ${styles.searchDiv}`}>
+                            <form class="d-flex" role="search">
+                                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                                <button class="btn btn-primary" type="submit"><BiSearch /></button>
+                            </form>
+                        </div>
+                        <div className={`col-6 ${styles.nextPageDiv}`}>
+                            <p>Total <span className={styles.nextPageSpan}>5</span></p>
+                            <p><GrFormPreviousLink className={styles.nextPrevIcon} /> <span className={styles.nextPageSpan}>1</span> <GrFormNextLink className={styles.nextPrevIcon} /></p>
+                        </div>
+                    </div>
+
+                    <div className="tab-content">
+                        <div className="tab-pane fade show active" id="menu0" role="tabpanel" aria-labelledby="menu0-tab">
+                            <div className={styles.contentTable}>
+                                <div className={`container ${styles.contentTableHead}`}>
+                                    <div className="row">
+                                        <div className='col-1'>#</div>
+                                        <div className='col-2'>Name</div>
+                                        <div className='col-2'>Department</div>
+                                        <div className='col-2'>Phone</div>
+                                        <div className='col-3'>Email</div>
+                                        <div className='col-2'>Option</div>
+                                    </div>
+                                </div>
+                                <div className={styles.contentTableBody}>
+                                    <div className="row">
+                                        <div className='col-1'>1</div>
+                                        <div className='col-2'>Muhammad Aminu</div>
+                                        <div className='col-2'>Surgery</div>
+                                        <div className='col-2'>0905 606 2376</div>
+                                        <div className='col-3'>docaminu@gmail.com</div>
+                                        <div className='col-2'>
+                                            <button type="button" class="btn btn-primary"><FiEdit2 /> Edit</button>
+                                            <button type="button" class="btn btn-danger">Delete <BsTrashFill /></button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={styles.contentTableBody}>
+                                    <div className="row">
+                                        <div className='col-1'>2</div>
+                                        <div className='col-2'>Muhammad Nasiru</div>
+                                        <div className='col-2'>Anaesthesiology</div>
+                                        <div className='col-2'>0813 633 5154</div>
+                                        <div className='col-3'>docnas@gmail.com</div>
+                                        <div className='col-2'>
+                                            <button type="button" class="btn btn-primary"><FiEdit2 /> Edit</button>
+                                            <button type="button" class="btn btn-danger">Delete <BsTrashFill /></button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={styles.contentTableBody}>
+                                    <div className="row">
+                                        <div className='col-1'>3</div>
+                                        <div className='col-2'>Muhammad Ibrahim</div>
+                                        <div className='col-2'>Pathology</div>
+                                        <div className='col-2'>0905 292 7094</div>
+                                        <div className='col-3'>docibrahim@gmail.com</div>
+                                        <div className='col-2'>
+                                            <button type="button" class="btn btn-primary"><FiEdit2 /> Edit</button>
+                                            <button type="button" class="btn btn-danger">Delete <BsTrashFill /></button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={styles.contentTableBody}>
+                                    <div className="row">
+                                        <div className='col-1'>4</div>
+                                        <div className='col-2'>Abraham Kamba</div>
+                                        <div className='col-2'>Gynaecology</div>
+                                        <div className='col-2'>0905 292 5643</div>
+                                        <div className='col-3'>dockamba@gmail.com</div>
+                                        <div className='col-2'>
+                                            <button type="button" class="btn btn-primary"><FiEdit2 /> Edit</button>
+                                            <button type="button" class="btn btn-danger">Delete <BsTrashFill /></button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={styles.contentTableBody}>
+                                    <div className="row">
+                                        <div className='col-1'>5</div>
+                                        <div className='col-2'>Kenneth Oche</div>
+                                        <div className='col-2'>Cardiology</div>
+                                        <div className='col-2'>0812 292 5643</div>
+                                        <div className='col-3'>dockenny@gmail.com</div>
+                                        <div className='col-2'>
+                                            <button type="button" class="btn btn-primary"><FiEdit2 /> Edit</button>
+                                            <button type="button" class="btn btn-danger">Delete <BsTrashFill /></button>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+
+
+
+
+
+
+                            </div>
+                        </div>
+
+                        <div className="tab-pane fade" id="menu1" role="tabpanel" aria-labelledby="menu1-tab">
+                            <h3>Add doctor</h3>
+                            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                    </div>
+                </div>
+
                 {/* body content end */}
             </div>
         </div>
