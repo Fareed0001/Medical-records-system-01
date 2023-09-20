da5283e2eea44417a02db45c41ac8feb7f01c885
Farid <fareedibrahim0003@gmail.com>
2023-09-20 21:02:05 +0100

    mobile sidebar

--- src/components/Navbar/AdminNavbar/Navbar.jsx
+++ src/components/Navbar/AdminNavbar/Navbar.jsx
@@ -3,20 +3,33 @@ import styles from '../Navbar.module.css';
 import MobileSidebar from '@/components/Navbar/AdminNavbar/AdminMobileSidebar/MobileSidebar';
 import { FaBars } from "react-icons/fa6";
 import { MdAdminPanelSettings } from "react-icons/md";
+import Link from "next/link";
 
 const Navbar = () => {
     return (
         <div className={`container ${styles.navbar}`}>
             <button className={`btn btn-primary ${styles.offCanvas}`} type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions"><FaBars /></button>
-            <div className="offcanvas offcanvas-start" data-bs-scroll="true" tabIndex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
-                <MobileSidebar />
+            <div className={`offcanvas offcanvas-start ${styles.offCanvasWidth}`} data-bs-scroll="true" tabIndex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
+                <div className={`offcanvas-header ${styles.offcanvasHeader}`}>
+                    <Link href="/" className={styles.offCanvasImageLink}>
+                        <img src="..\images\moreed-black-logo.png" alt="moreed-logo" />
+                    </Link>
+                    <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div className="offcanvas-body">
+                    <MobileSidebar />
+                </div>
             </div>
+
             <p className={styles.greeting}>
-            <span className={styles.username}>Hi Farid Ibrahim</span> <br />
+                <span className={styles.username}>Hi Farid Ibrahim</span> <br />
                 <span className={styles.welcome}>Welcome back</span></p>
 
-            <p className={styles.role}><span className={styles.icon}>
-                <MdAdminPanelSettings /></span> Admin</p>
+            <Link href="/" className={styles.roleLink}>
+                <p className={styles.role}><span className={styles.icon}>
+                    <MdAdminPanelSettings /></span> Admin</p>
+            </Link>
+
         </div>
     );
 }
